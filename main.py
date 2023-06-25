f11a3aefa5b8c43fc564666d322f510ca28885b9
VyomKapur <vkapur_be20@thapar.edu>
2023-06-25 17:08:50 +0530

    added class for objects

--- main.py
+++ main.py
@@ -2,18 +2,52 @@ import cv2
 from ultralytics import YOLO
 import supervision as sv
 
+MODEL_NAME = "yolov8s.pt"
 
-def main():
-    model = YOLO("yolov8l.pt")
-    
-    boxannotator = sv.BoxAnnotator(
+class detected_obj:
+    def __init__(self, detection):
+        self.y_mid = (detection[0][1] + detection[0][3]) / 2
+        self.xyxy = detection[0]
+        self.confidence = detection[1]
+        self.class_id = detection[2]
+        self.tracker_id = detection[3]
+        self.id = f"{self.class_id}_{self.tracker_id}"
+        self.inside = 0
+        self.gradient = 0
+
+def model_import(file_name):
+    return YOLO(file_name)    
+
+def get_boxannotator() -> sv.BoxAnnotator:
+    return sv.BoxAnnotator(
         thickness=2,
         text_thickness=2,
         text_scale=1
     )
-    inside_outside = {}
+
+def get_labels(detections, model) -> list[str]:
+    return [
+            f"{tracker_id} {model.model.names[class_id]} {confidence: 0.2f}"
+            for _, confidence, class_id, tracker_id
+            in detections
+        ]
+
+def get_annotations(boxannotator, frame, detections, labels):
+    return boxannotator.annotate(
+            scene=frame, 
+            detections=detections,
+            labels=labels
+        )
+
+def main():
+    
+    model = model_import(MODEL_NAME)
+
+    boxannotator = get_boxannotator()
     inside_cart = {}
+    objs = {}
     for result in model.track(source=0, stream=True):
+
         frame = result.orig_img
         detections = sv.Detections.from_yolov8(result)        
         
@@ -22,44 +56,34 @@ def main():
         
         detections = detections[detections.class_id!=0]
         
-        labels = [
-            f"{tracker_id} {model.model.names[class_id]} {confidence: 0.2f}"
-            for _, confidence, class_id, tracker_id
-            in detections
-        ]
+        labels = get_labels(detections, model)
 
-        frame = boxannotator.annotate(
-            scene=frame, 
-            detections=detections,
-            labels=labels
-        )
+        frame = get_annotations(boxannotator, frame, detections, labels)
 
-        cv2.line(frame, (0, 240), (640, 240), (255, 255, 255), thickness=2)
-        print(detections)
-        for detection in detections:
-            if detection[1] == 0:
-                continue
-
-            if detection[3] not in inside_outside.keys():
-                if detection[0][1] + detection[0][3] // 2 < 240:
-                    inside_outside[detection[3]] = 1
-                    inside_cart[detection[2]] = inside_cart.get(detection[2], 0) + 1
-                
-            else:
-                if detection[0][1] + detection[0][3] // 2 > 240 and inside_outside[detection[3]] == 1:
-                    inside_outside[detection[3]] = 0
-                    if detection[2] in inside_cart.keys():
-                        inside_cart[detection[2]] -= 1
-                    if inside_cart[detection[2]] < 1:
-                        del inside_cart[detection[2]]
-                    
-
-        print(inside_outside)
+        cv2.line(frame, (0, 240), (640, 240), (0, 0, 0), thickness=3)
         
-        print(inside_cart)
+        for detection in detections:
+            obj = detected_obj(detection)
 
+            if obj.id not in objs.keys():
+                objs[obj.id] = obj
+
+            else:
+                old_obj = objs[obj.id]
+                if obj.y_mid > 240 and old_obj.y_mid < 240:
+                    # incoming
+                    inside_cart[obj.class_id] = inside_cart.get(obj.class_id, 0) + 1
+
+                elif obj.y_mid < 240 and old_obj.y_mid > 240:
+                    inside_cart[obj.class_id] = inside_cart.get(obj.class_id, 0) -1
+                    if inside_cart[obj.class_id] < 1:
+                        del inside_cart[obj.class_id]
+                    # outgoing                
+                objs[obj.id] = obj
+
+        print(inside_cart)
         cv2.imshow("capture", frame)
-        if(cv2.waitKey(30) == 27):
+        if(cv2.waitKey(30) == 27): 
             break
 
 if __name__ == "__main__":
